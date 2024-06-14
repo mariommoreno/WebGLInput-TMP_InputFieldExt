fb7a2d7d141897d20caa6108056d239aeba8598e
Mario Morales <mario.morales.moreno@gmail.com>
2024-06-14 19:42:40 +0200

    Initial
    Tested with WebGLVersion 1.4.0 and TextMeshPro 3.0.9 on Unity v2023.3.32f1

---
+++ TMP_InputFieldExt.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+
+namespace TMPro
+{
+    /// <summary>
+    /// Editable text input field.
+    /// </summary>
+    [AddComponentMenu("UI/TextMeshPro - Input Field EXT", 11)]
+    public class TMP_InputFieldExt : TMP_InputField
+    {
+		
+        [Serializable]
+        public class OnPositionChangedEvent : UnityEvent<int> { }
+
+
+        /// <summary>
+        /// Event delegates triggered when carret or string position changed on click.
+        /// </summary>
+        [SerializeField]
+        private OnPositionChangedEvent m_OnPositionChanged = new OnPositionChangedEvent();
+
+
+        public OnPositionChangedEvent onPositionChanged { get { return m_OnPositionChanged; } set { SetPropertyUtilityExt.SetClass(ref m_OnPositionChanged, value); } }
+
+
+        protected void SendOnPositionChanged()
+        {
+            if (onPositionChanged != null)
+                onPositionChanged.Invoke(stringPositionInternal);
+        }
+
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            base.OnPointerDown(eventData);
+
+            SendOnPositionChanged();
+        }
+
+        static class SetPropertyUtilityExt
+        {
+            public static bool SetClass<T>(ref T currentValue, T newValue) where T : class
+            {
+                if ((currentValue == null && newValue == null) || (currentValue != null && currentValue.Equals(newValue)))
+                    return false;
+
+                currentValue = newValue;
+                return true;
+            }
+        }
+    }
+}
---
+++ WrappedTMPInputFieldExt.cs
@@ -0,0 +1,37 @@
+﻿#if UNITY_2018_2_OR_NEWER
+#define TMP_WEBGL_SUPPORT
+#endif
+
+#if TMP_WEBGL_SUPPORT
+using UnityEngine;
+using TMPro;
+using WebGLSupport.Detail;
+using UnityEngine.UI;
+using System;
+using System.Transactions;
+
+namespace WebGLSupport
+{
+    /// <summary>
+    /// Wrapper for TMPro.TMP_InputField
+    /// </summary>
+    class WrappedTMPInputFieldExt : WrappedTMPInputField
+    {
+        TMP_InputFieldExt input;
+
+        public WrappedTMPInputFieldExt(TMP_InputFieldExt input): base(input) 
+        {
+            this.input = input;
+        }
+
+        private TMP_InputFieldExt.OnPositionChangedEvent _onPositionChanged;
+
+        public TMP_InputFieldExt.OnPositionChangedEvent OnPositionChanged
+        {
+            get => input.onPositionChanged;
+        }
+       
+    }
+}
+
+#endif // TMP_WEBGL_SUPPORT
